ea58bc6cee1e067c7e18c010865b4f55765c3ac1
Bogdan Catalin Iacob <bogdancatalin.iacob@gmail.com>
2022-02-02 01:39:34 +0000

    Get elements from the DOM and assign them to variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
+const width = 10; //number of squares on a row
+const topScore = document.getElementById("top-score");
+const score = document.getElementById("score");
+const level = document.getElementById("level");
+const playButton = document.getElementById("play-button");
+const soundsButton = document.getElementById("sounds-button");
 const gridContainer = document.getElementById("grid-container");
+
+createGridDivs();
+
+const getSquares = () => Array.from(
+    document.getElementById("grid-container")
+    .getElementsByTagName("div"));
 
 /**
  * Create 200 divs for the grid layout
  */
-function createGridDivs (){
+function createGridDivs() {
     for (let i = 0; i < 200; i++) {
         gridContainer.innerHTML += `<div class="square"></div>`
     }
 };
 
-createGridDivs();
+console.log(getSquares());
